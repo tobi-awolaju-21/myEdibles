9caa64cde8a2af5d1bc0a3c4a0784c118d10a2aa
Tobi <tobiawolaju21@gmail.com>
2024-03-10 21:58:36 +0100

    ugooues

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
     function nextPage(pageName) {
         // You can customize the navigation logic here
         // For example, you can use window.location.href to navigate to a specific page
+        window.history.back();
         window.location.href = pageName+'.html';
     }
